716580866038203aac0804e27157503296fe421f
Pravin Patel <patelpra337@gmail.com>
2019-10-01 21:56:21 -0500

    Added numbers to the complex view controllers

--- Screens_/NumberedViewController.swift
+++ Screens_/NumberedViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class NumberedViewController: UIViewController {
+    
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 160, height: 80))
 
+    // This method is called
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(label)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 64)
+        label.center = view.center
+        
+    // label.backgroundColor = .brown // make sure view is where it's supposed to be
 
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let number = navigationController?.viewControllers.count {
+            label.text = "\(number)"
+          //  label.text = String(number)
+        }
+    }
 }
